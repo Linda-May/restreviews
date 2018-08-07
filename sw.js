7fc5dfcb1f8e313f12c47396a4960e6efdf4b5d1
Linda-May <32400035+Linda-May@users.noreply.github.com>
2018-08-07 13:34:16 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,47 +1,60 @@
-self.addEventListener('install', (event) => {
- event.waitUntil(
-   caches.open('restaurant').then((cache) => {
-     return cache.addAll([
-       '/',
-       '/index.html',
-       '/restaurant.html',
-       '/css/styles.css',
-       '/js/dbhelper.js',
-       '/js/main.js',
-       '/js/restaurant_info.js',
-       '/data/restaurants.json',
-       '/img/',
-       '/img/1.jpg',
-       '/img/2.jpg',
-       '/img/3.jpg',
-       '/img/4.jpg',
-       '/img/5.jpg',
-       '/img/6.jpg',
-       '/img/7.jpg',
-       '/img/8.jpg',
-       '/img/9.jpg',
-       '/img/10.jpg',
-     ]).then(() => {
-      console.log('Finished caching all files!');
-     }).catch((error) => {
-      console.log('Caching threw an error: ', error);
-     })
-   })
- );
+const CACHE_NAME = 'restaurantsPWA';
+const DATA_CACHE_NAME = 'restaurantPWA_DATA';
+
+
+
+self.addEventListener('install', function(event) {
+  console.log('sw install');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(
+        [
+          '/js/dbhelper.js',
+          '/js/restaurant_info.js',
+          '/js/main.js',
+          '/css/styles.css',
+          '/data/restaurants.json',
+          '/index.html',
+          '/restaurant.html',
+          './img/1.jpg',
+          './img/2.jpg',
+          './img/3.jpg',
+          './img/4.jpg',
+          './img/5.jpg',
+          './img/6.jpg',
+          './img/7.jpg',
+          './img/8.jpg',
+          './img/9.jpg',
+          './img/10.jpg'
+        ]
+      );
+    })
+  );
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('Activating service worker...');
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
-  // Do not use service worker for the Google Maps API
-  if (event.request.url.indexOf('maps.googleapis.com') !== -1) return;
+self.addEventListener('fetch', function(event) {
+  console.log('sw fetch');
+
 
   event.respondWith(
-    caches.open('restaurant').then((cache) => {
-      return cache.match(event.request).then((response) => {
-        return response || fetch(event.request).then((response) => {
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
           cache.put(event.request, response.clone());
           return response;
         });
